a92ab7e1f53b91c67f2792fa400a9c77ec47fd31
Jose Cruz <cruzjosework@gmail.com>
2020-09-13 02:01:37 -0400

    added a joke command that will tap into joke api and return a joke after command.

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const Discord = require("discord.js");
-
+const fetch = require('node-fetch')
 const config = require('./config.json');
+
 const bot = new Discord.Client();
+
 
 bot.on('ready', () => {
   console.log('bot is ready')
@@ -47,6 +49,30 @@ bot.on('message', async (msg) => {
     msg.channel.bulkDelete(num);
     //notify channel of deleted messages
     msg.channel.send(`deleted  ${args[0]} posts for you`);
+  }
+
+  if(command === "welcome") {
+    msg.reply(' @guardian_angel get off my beef')
+  }
+
+  if(command === 'joke') {
+    //async API call using async/await syntax
+    let getJoke = async () => {
+      //make API call
+      let result = await fetch('https://official-joke-api.appspot.com/random_joke')
+      //convert to object we can work with
+      let json = await result.json()
+      return json
+    }
+    //call function defined above
+    let joke = await getJoke()
+    
+    //have our bot reply using the data returned from our API call
+    msg.reply(`
+    Here's your joke
+    ${joke.setup}
+    ${joke.punchline}
+    `)
   }
   
 })
